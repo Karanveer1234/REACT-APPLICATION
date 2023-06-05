1d077142c0375653b5d6a9212ae9aa0b962a30e2
ktsingh <ktsingh@gerrit.dminc-gtc.com>
2023-06-05 19:41:53 +0530

    header changes

--- src/registration/Header.js
+++ src/registration/Header.js
@@ -1,6 +1,5 @@
 import React from 'react';
 function Header(props) {
-   
     return(
         <nav class="navbar-dark">
             <div className="row col-12 d-flex justify-content-center text-white" style={{backgroundColor:'Wheat'}}>
