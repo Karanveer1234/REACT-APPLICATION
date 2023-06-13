5677f7edfc0b4a76c88fbeccf38742c33bfa8cd6
Karanveer <karanveerthakur057@gmail.com>
2023-06-13 18:51:37 +0530

    add alert

--- src/resume.js
+++ src/resume.js
@@ -1,17 +1,10 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
 
 try {
   // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  const payload2 = payload["karan"];
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
+  adddlert("Welcome guest!");
 
+} catch (error) {
+  console.log("inside catch block")
+  core.setFailed(error.message);
 }
